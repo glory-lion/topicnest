2129483312b37d05b99e7a2ba5060b4613d14cbc
Glory Lion <glorylion@Glorys-MacBook-Pro.local>
2026-01-04 13:32:03 +0800

    update

--- src/components/index.ts
+++
@@ -1,7 +0,0 @@
-export { default as Header } from './Header';
-export { default as Sidebar } from './Sidebar';
-export { default as RightSidebar } from './RightSidebar';
-export { default as PostCard } from './PostCard';
-export { default as FeedSort } from './FeedSort';
-export { default as CreatePostWidget } from './CreatePostWidget';
-export * from './Icons';
